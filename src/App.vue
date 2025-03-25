f6d4cb33d97511cdf02b01df1b5034902eaa5b11
Jont251 <nevgeny90@gmail.com>
2025-03-25 14:33:00 +0200

    updated header

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup>
 import './assets/base.css'
-import Header from './components/Header.vue'
+import Header from './components/Header/Header.vue'
 import MainInfo from './components/MainInfo.vue'
 import Connect from './components/Connect.vue'
 import Footer from './components/Footer.vue'
---
+++ src/components/Header/Header.vue
@@ -0,0 +1,36 @@
+<template>
+  <header class="header">
+    <div class="header-content">
+      <h1 class="slogan">
+        <div class="slogan-main">Мгновенные мультивалютные обмены</div>
+        <div class="slogan-location">Просто, быстро, надежно в любом городе Израиля</div>
+
+        <div class="slogan-features">
+          <span class="feature">Шекели</span>
+          <span class="dot">•</span>
+          <span class="feature">Рубли</span>
+          <span class="dot">•</span>
+          <span class="feature">Доллары</span>
+          <span class="dot">•</span>
+          <span class="feature">Евро</span>
+          <span class="dot">•</span>
+          <span class="feature">USDT</span>
+        </div>
+        <div class="slogan-location">Наличные и безналичные в банках РФ</div>
+
+      </h1>
+    </div>
+    <div class="abstract-shapes">
+      <div class="shape shape-1"></div>
+      <div class="shape shape-2"></div>
+      <div class="shape shape-3"></div>
+    </div>
+  </header>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+@import './Header.css';
+</style>
